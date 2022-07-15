29156e5cd8e3ba7106820c6d13a23dad2b8ac29d
cairon-henrique-60 <caironhenrique60@gmail.com>
2022-07-15 20:21:14 -0300

    Classe tabuleiro Classe Pecas

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import boardgame.Tabuleiro;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		
+		
+	}
+
+}
