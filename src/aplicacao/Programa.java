7a6a61729fcf6b33ae900807fd8374edc6ed4190
cairon-henrique-60 <caironhenrique60@gmail.com>
2022-07-15 19:44:26 -0300

    Refatorando

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,12 @@
+package aplicacao;
+
+import boardgame.Position;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Position pos = new Position(2, 5);
+		System.out.println(pos);
+	}
+
+}
