851734020551bdf016a98d6379f2e49df02ef97b
cairon-henrique-60 <caironhenrique60@gmail.com>
2022-07-18 22:59:36 -0300

    Foi criado o metodo para converter um char E foi criado as excecoes personalizadas do classe Xadrez

--- src/xadrez/PartidaDeXadez.java
+++ src/xadrez/PartidaDeXadez.java
@@ -1,6 +1,5 @@
 package xadrez;
 
-import boardgame.Position;
 import boardgame.Tabuleiro;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
@@ -23,10 +22,13 @@ public class PartidaDeXadez {
 		return mat;
 	}
 	
-	private void iniciarPartida() {
-		tabuleiro.lugarDaPeca(new Torre(tabuleiro, Cores.WHITE), new Position(2, 1));
-		tabuleiro.lugarDaPeca(new Rei(tabuleiro, Cores.BLACK), new Position(0, 4));
-		tabuleiro.lugarDaPeca(new Rei(tabuleiro, Cores.BLACK), new Position(7, 4));
-
+	private void colocandoNovaPeca(char coluna, int linha, PecaDeXadrez peca) {
+		tabuleiro.lugarDaPeca(peca, new PosicaoXadrez(coluna, linha).toPosition());
 	}
+	
+	private void iniciarPartida() {
+		colocandoNovaPeca('b', 6, new Torre(tabuleiro, Cores.WHITE));
+		colocandoNovaPeca('e', 8, new Rei(tabuleiro, Cores.BLACK));
+		colocandoNovaPeca('e', 1, new Rei(tabuleiro, Cores.WHITE));
+		}
 }
---
+++ src/xadrez/PosicaoXadrez.java
@@ -0,0 +1,36 @@
+package xadrez;
+
+import boardgame.Position;
+
+public class PosicaoXadrez {
+	private char coluna;
+	private int linha;
+	
+	public PosicaoXadrez(char coluna, int linha) {
+		if (coluna < 'a' || coluna > 'h' || linha < 1 || linha > 8) {
+			throw new ExcecoesXadrez("Erro instanciando PosicaoXadrez. Valores validos de a1 há h8");
+		}
+		this.coluna = coluna;
+		this.linha = linha;
+	}
+	public char getColuna() {
+		return coluna;
+	}
+	
+	public int getLinha() {
+		return linha;
+	}
+	
+	protected Position toPosition() {
+		return new Position(8 - linha, coluna - 'a');
+	}
+	
+	protected static PosicaoXadrez haPosicao(Position position) {
+		return new PosicaoXadrez((char)('a' - position.getColuna()), 8 - position.getLinha());
+	}
+	
+	@Override
+	public String toString() {
+		return "" + coluna + linha;
+	}
+}
